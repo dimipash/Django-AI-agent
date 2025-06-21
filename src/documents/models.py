02b9fcc58c81d04a7dcae2ae8a8489a2bbd5a4f5
Dimitar Pashev <dim.pashev@gmail.com>
2025-06-21 14:42:57 +0300

    add users and admin

--- src/documents/models.py
+++ src/documents/models.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.db import models
+from django.utils import timezone
+
 
 USER = settings.AUTH_USER_MODEL
 
@@ -9,8 +11,16 @@ class Document(models.Model):
     title = models.CharField(default="Title")
     content = models.TextField(blank=True, null=True)
     active = models.BooleanField(default=True)
+    active_at = models.DateTimeField(auto_now_add=False, auto_now=False, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.active and self.active_at is None:
+            self.active_at = timezone.now()
+        else:
+            self.active_at = None
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
