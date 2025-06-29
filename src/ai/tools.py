cb638af6ebacb84af557763f7023f65c55f00230
Dimitar Pashev <dim.pashev@gmail.com>
2025-06-29 15:24:17 +0300

    add Django-based LangGraph agent

--- src/ai/tools.py
+++ src/ai/tools.py
@@ -7,20 +7,16 @@ from documents.models import Document
 @tool
 def list_documents(config: RunnableConfig):
     """
-    List the most recent LIMIT documents for the current user with maximum of 25.
-
-    agruments
-    limit: number of results
+    List the most recent 5 documents for the current user
     """
-    print(config)
-    metadata = config.get("metadata") or config.get("configurable")
-    user_id = metadata.get("user_id")
+    limit = 5
+    configurable = config.get("configurable") or config.get("metadata")
+    user_id = configurable.get("user_id")
     if not user_id:
         raise Exception("User not found")
-    print(user_id)
-    qs = Document.objects.filter(owner_id=user_id, active=True)
+    qs = Document.objects.filter(owner_id=user_id, active=True).order_by("-created_at")
     response_data = []
-    for obj in qs:
+    for obj in qs[:limit]:
         response_data.append({"id": obj.id, "title": obj.title})
 
     return response_data
@@ -31,8 +27,8 @@ def get_document(document_id: int, config: RunnableConfig):
     """
     Get the details of a document for the current user
     """
-    metadata = config.get("metadata") or config.get("configurable")
-    user_id = metadata.get("user_id")
+    configurable = config.get("configurable") or config.get("metadata")
+    user_id = configurable.get("user_id")
     if not user_id:
         raise Exception("User not found")
 
@@ -42,7 +38,10 @@ def get_document(document_id: int, config: RunnableConfig):
         raise Exception("Document not found")
     except:
         raise Exception("Something went wrong")
-    
+
     response_data = {"id": obj.id, "title": obj.title}
 
     return response_data
+
+
+document_tools = [list_documents, get_document]
