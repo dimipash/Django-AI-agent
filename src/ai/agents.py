d0ad1305105c6cf3aba8525688188643f460d81f
Dimitar Pashev <dim.pashev@gmail.com>
2025-07-11 17:24:10 +0300

    add API-driven AI agent

--- src/ai/agents.py
+++ src/ai/agents.py
@@ -1,7 +1,8 @@
 from langgraph.prebuilt import create_react_agent
 from ai.llms import get_openai_model
 from ai.tools import (
-    document_tools
+    document_tools,
+    movie_discovery_tools,
     )
 
 
@@ -11,6 +12,18 @@ def get_document_agent(model=None, checkpointer=None):
     agent = create_react_agent(
         model=llm_model,
         tools=document_tools,
+        prompt="You are a helpful assistant in managing a user's documents within this app.",
+        checkpointer=checkpointer
+    )
+
+    return agent
+
+def get_movie_discovery_agent(model=None, checkpointer=None):
+    llm_model = get_openai_model(model=model)
+
+    agent = create_react_agent(
+        model=llm_model,
+        tools=movie_discovery_tools,
         prompt="You are a helpful assistant in finding and discovering information about movies.",
         checkpointer=checkpointer
     )
