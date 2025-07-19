7cef284fb1b9573f06d7dd30a2b4c8df31a4127d
Dimitar Pashev <dim.pashev@gmail.com>
2025-07-19 13:39:35 +0300

    integrate Permit.iuo with the Django AI agent

--- src/home/settings.py
+++ src/home/settings.py
@@ -116,4 +116,8 @@ OPENAI_API_KEY = config('OPENAI_API_KEY', default=None)
 
 TMDB_API_KEY = config('TMDB_API_KEY', default=None)
 
+PERMIT_API_KEY=config('PERMIT_API_KEY', default=None)
+
+PERMIT_PDP_URL=config('PERMIT_PDP_URL', default="https://cloudpdp.api.permit.io")
+
 
